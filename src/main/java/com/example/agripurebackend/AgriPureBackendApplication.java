d412fa1790c4a9398316b44da499db0ca5c2401e
FranchescoSoto <u201821078@upc.edu.pe>
2023-10-09 19:18:47 -0500

    created project

---
+++ src/main/java/com/example/agripurebackend/AgriPureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.agripurebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriPureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriPureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agripurebackend/AgriPureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agripurebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriPureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
